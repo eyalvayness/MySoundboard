74aa1b4c65dc1e3cb15efe7e0eb8133d4b161ac0
Eyal Vayness <evayness@gmail.com>
2020-05-20 17:29:05 +0200

    initial commit

---
+++ Soundboard/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using Soundboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Soundboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Sound> filenames;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            filenames = new ObservableCollection<Sound>();
+
+            DG.DataContext = filenames;
+        }
+
+        private void AddNewClick(object sender, RoutedEventArgs e)
+        {
+            var d = new OpenFileDialog
+            {
+                Filter = "mp3 files (*.mp3)|*.mp3|All files (*.*)|*.*",
+                Title = "Pick the sound to add to the sound board",
+                Multiselect = false
+            };
+
+            if (d.ShowDialog() == true)
+            {
+                if (!d.FileName.EndsWith(".mp3"))
+                    return;
+
+                Sound s = new Sound(d.FileName, d.SafeFileName);
+                filenames.Add(s);
+            }
+        }
+    }
+}
---
+++ Soundboard/Models/Sound.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soundboard.Models
+{
+    public class Sound
+    {
+        public string Path { get; set; }
+        public string Name { get; set; }
+
+        public Sound(string path, string name)
+        {
+            Path = path;
+            Name = name;
+        }
+    }
+}
